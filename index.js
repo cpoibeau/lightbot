55b1b0e386d7a18e6d2c5f27205c2a0a514fd144
ArcLight <poibeau.clement@gmail.com>
2018-08-12 13:01:21 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const discord = require('discord.js');
+const fs = require('fs');
+let bot = new discord.Client();
+
+var prefix = 'lb-';
+
+//Ready + liste des seveurs
+bot.on('ready', () => {
+    console.log('Je suis pret');
+    let guildsId = Array.from(bot.guilds.keys());
+    guildsId.forEach(element => {
+        console.log(bot.guilds.get(element).name);
+    });
+});
+
+//Modification du préfixe
+bot.on('message', (msg) => {
+    if(msg.content.includes(prefix + 'setPrefix')){
+        let parsedString = msg.content.split(' ');
+        prefix = parsedString[1];
+        msg.channel.send(`Bot prefix has been set to : ${prefix}`)
+    }
+})
+
+//Ping
+bot.on('message', (msg) => {
+    if(msg.content.includes(prefix + 'ping')){
+        msg.channel.send('`pong !`');
+    }
+});
+
+//Message de bienvenue
+bot.on('guildMemberAdd', (member) => {
+    member.send('Bienenue sur le serveur ' + member.user.username + ' !');
+    member.guild.channels.get('477948088233754624').send('Bienvenue sur le serveur <@' + member.user.id + '> !');
+});
+
+//Purge des messages
+/*
+bot.on('message', (msg) => {
+    if(msg.content === '!purge'){
+        let messages = msg.channel.fetchMessages();
+        messages.catch
+    }
+});
+*/
+
+
+bot.login('NDc3ODgwNTQzODU4MzkzMTI4.DlDWmg.y0HjHNMdr76pcU2kcoeWk-tHJRk');
