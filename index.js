7e44b594c61a44e459caf1e3c7c2bc66ef760249
ArcLight <poibeau.clement@gmail.com>
2018-08-14 01:00:32 +0200

    Updated comments, added DM commands

--- index.js
+++ index.js
@@ -9,14 +9,17 @@ let welcomeChannel;
 
 // Ready + liste des seveurs
 client.on('ready', () => {
-    console.log('Ready !');
-    Array.from(client.guilds.keys()).forEach((element) => {
-        console.log(`> ${client.guilds.get(element).name}`);
-    });
-    client.user.setActivity(`Say ${prefix}help for help !`, {
-        url: 'https://discord.js.org',
-        type: 'PLAYING'
-    });
+  console.log('Ready !');
+  Array.from(client.guilds.keys()).forEach((element) => {
+    console.log(`> ${client.guilds.get(element).name}`);
+  });
+  client.user.setActivity(`Say ${prefix}help for help !`, {
+    url: 'https://discord.js.org',
+    type: 'PLAYING'
+  });
+  
+  //client.guilds.find('name', 'Test bot').members.array() FIXME : complete the DM reset function
+  
 });
 
 function adminCheckFromMsg(msg){
@@ -26,38 +29,59 @@ function adminCheckFromMsg(msg){
 
 // Commands
 client.on('message', (msg) => {
-  if (msg.author != client.user) {
+  if (msg.author != client.user) {  // FIXME : check for all bots, not only this one
     let command = new TextCommand(prefix, msg);
     let reg = new RegExp('^' + prefix, 'i');
-    if (msg.content.includes(prefix + 'help')) { // help
-      command.help();
+    if(msg.guild != undefined){   // DM commands
+      if (msg.content.includes(prefix + 'help')) {
+        command.help();
 
-    } else if (msg.content.includes(prefix + 'ping')) { // ping
-      command.ping();
+      } else if (msg.content.includes(prefix + 'ping')) {
+        command.ping();
+        
+      } else if (msg.content.includes(prefix + 'search')) {
+        command.search();
+
+      } else if (msg.content.includes(prefix + 'population')){
+        command.population();
       
-    } else if (msg.content.includes(prefix + 'search')) {   // search
-      command.search();
+      } else if(adminCheckFromMsg(msg)) { // admin commands
+        if (msg.content.includes(prefix + 'setPrefix')) {
+          prefix = command.setPrefix();
 
-    } else if (msg.content.includes(prefix + 'population')){    // population
-      command.population();
-    
-    } else if(adminCheckFromMsg(msg)) { // admin commands
-      if (msg.content.includes(prefix + 'setPrefix')) {   // setPrefix
-        prefix = command.setPrefix();
+        } else if (msg.content.includes(prefix + 'purge')){
+          command.purge();
+        
+        } else if (msg.content.includes(prefix + 'setWelcomeChannel')) {
+          welcomeChannel = command.setWelcomeChannel();
 
-      } else if (msg.content.includes(prefix + 'purge')){ // purge
-        command.purge();
-      
-      } else if (msg.content.includes(prefix + 'setWelcomeChannel')) {    // setWelcomeChannel
-        welcomeChannel = command.setWelcomeChannel();
-        console.log(welcomeChannel);
+        } else if(reg.test(msg.content)){
+          msg.channel.send("Command not found :/");
+        }
 
       } else if(reg.test(msg.content)){
-        msg.channel.send("Command not found :/");
+        msg.channel.send("Command not found, you may not be allowed to use it :/");
       }
+      
+    } else {  // DM commands
+      if (msg.content.includes(prefix + 'help')) {
+        command.help();
 
-    } else if(reg.test(msg.content)){
-      msg.channel.send("Command not found, you may not be allowed to use it :/");
+      } else if (msg.content.includes(prefix + 'ping')) {
+        command.ping();
+        
+      } else if (msg.content.includes(prefix + 'search')) {
+        command.search();
+
+      } else if (msg.content.includes(prefix + 'setPrefix')) {
+        prefix = command.setPrefix();
+
+      } else if (msg.content.includes(prefix + 'purge')){
+        command.purge();
+      
+      } else if(reg.test(msg.content)){
+        msg.channel.send("Command not found, you may have entered a command only available on a Discord server :/");
+      }
     }
   }
 });
