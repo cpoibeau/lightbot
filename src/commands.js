1ffb58614ed544978fa67bc1a13c633ebfe7bbdc
Clément <poibeau.clement@gmail.com>
2018-12-26 10:58:30 +0100

    Added transfer command

--- src/commands.js
+++ src/commands.js
@@ -129,7 +129,7 @@ module.exports = class TextCommand {
   
           if(this.message.guild.members.find('id', regex.exec(this.message.content)[1])){
             let user = this.message.guild.members.find('id', regex.exec(this.message.content)[1]);
-            db.query(`INSERT INTO users (discord_id, username, guild_id, guild_name) VALUES ('${user.id}', '${user.displayName}', '${this.message.guild.id}', '${this.message.guild.name}')`, (err)=> {
+            db.query(`INSERT INTO users (discord_id, username, guild_id, guild_name) VALUES ('${user.id}', '${user.displayName}', '${this.message.guild.id}', '${this.message.guild.name};')`, (err)=> {
               if(err){
                 this.message.channel.send('This account is already existing');
               } else {
@@ -151,7 +151,7 @@ module.exports = class TextCommand {
 
           if(this.message.guild.members.find('id', regex.exec(this.message.content))){
             let user = this.message.guild.members.find('id', regex.exec(this.message.content));
-            db.query(`SELECT balance FROM users WHERE discord_id='${user.id}' AND guild_id='${this.message.guild.id}'`, (err, result)=>{
+            db.query(`SELECT balance FROM users WHERE discord_id='${user.id}' AND guild_id='${this.message.guild.id};'`, (err, result)=>{
               
               if(err){
                 this.message.channel.send('There is no account for this user');
@@ -176,7 +176,7 @@ module.exports = class TextCommand {
           if(this.message.guild.members.find('id', regex.exec(this.message.content)[1])){
             let user = this.message.guild.members.find('id', regex.exec(this.message.content)[1]);
 
-            db.query(`SELECT balance FROM users WHERE discord_id='${user.id}' AND guild_id='${this.message.guild.id}'`, (err, result)=>{
+            db.query(`SELECT balance FROM users WHERE discord_id='${user.id}' AND guild_id='${this.message.guild.id};'`, (err, result)=>{
               if(err){
                 this.message.channel.send('Failed to credit this user');
 
@@ -194,7 +194,7 @@ module.exports = class TextCommand {
               }
             });
           } else {
-            this.message.channel.send('You must enter a valid name');
+            this.message.channel.send('You must enter a valid user tag');
           }
         } else {
           this.message.channel.send('You must enter a user tag and an amount');
@@ -209,7 +209,7 @@ module.exports = class TextCommand {
           if(this.message.guild.members.find('id', regex.exec(this.message.content)[1])){
             let user = this.message.guild.members.find('id', regex.exec(this.message.content)[1]);
             let amount = regex.exec(this.message.content)[2];
-            db.query(`UPDATE users SET salary=${amount} WHERE discord_id='${user.id}' AND guild_id='${this.message.guild.id}'`, (err)=>{
+            db.query(`UPDATE users SET salary=${amount} WHERE discord_id='${user.id}' AND guild_id='${this.message.guild.id};'`, (err)=>{
               
               if(err){
                 this.message.channel.send('Failed to set the salary of this user');
@@ -218,7 +218,7 @@ module.exports = class TextCommand {
               }
             });
           } else {
-            this.message.channel.send('You must enter a valid name');
+            this.message.channel.send('You must enter a valid user tag');
           }
         } else {
           this.message.channel.send('You must enter a user tag and an amount');
@@ -229,16 +229,72 @@ module.exports = class TextCommand {
         let user = this.message.guild.members.find('id', this.message.author.id);
 
         if(user.hasPermission('MANAGE_GUILD')) {
-          db.query(`UPDATE users SET balance=balance+salary`);
+          db.query(`UPDATE users SET balance=balance+salary;`);
         } else {
           this.message.channel.send('You\'re not allowed to use this command');
         }
-      } else {
-        this.message.channel.send('Invalid argument');
-      }
 
-    } else {
-      //TODO : display help
+      //transfer
+      } else if(this.message.content.split(' ')[1] == 'transfer') {
+        let regex = /<@!?(\d+)> <@!?(\d+)> (\d+)$/;
+
+        if(regex.test(this.message.content)) {
+
+          if(this.message.guild.members.find('id', regex.exec(this.message.content)[1])) {
+            let user1 = this.message.guild.members.find('id', regex.exec(this.message.content)[1]);
+
+            if((this.message.author.id == user1.id) || (this.message.guild.members.find('id', this.message.author.id).hasPermission('MANAGE_GUILD'))) {
+
+              if(this.message.guild.members.find('id', regex.exec(this.message.content)[2])) {
+                let user2 = this.message.guild.members.find('id', regex.exec(this.message.content)[2]);
+                db.query(`SELECT balance FROM users WHERE (discord_id='${user1.id}' OR discord_id='${user2.id}') AND guild_id='${this.message.guild.id}'`, (err, result)=>{
+
+                  if(err){
+                    this.message.channel.send('Failed to transfer this amount');
+
+                  } else if(result.length == 2){
+                    db.query(`START TRANSACTION;`);
+                    db.query(`UPDATE users SET balance=balance-${regex.exec(this.message.content)[3]} WHERE discord_id=${user1.id} AND guild_id=${this.message.guild.id};`, (err)=>{
+
+                      if(err){
+                        console.error(err);
+                        this.message.channel.send('Failed to transfer this amount');
+                        db.query(`ROLLBACK;`);
+
+                      } else {
+                        db.query(`UPDATE users SET balance=balance+${regex.exec(this.message.content)[3]} WHERE discord_id=${user2.id} AND guild_id=${this.message.guild.id};`, (err)=>{
+
+                          if(err){
+                            console.error(err);
+                            db.query(`ROLLBACK;`);
+                            this.message.channel.send('Failed to transfer this amount');
+
+                          } else {
+                            db.query(`COMMIT;`);
+                            this.message.channel.send('Successfully transfered this amount');
+                          }
+                        });
+                      }
+                    });
+                  } else {
+                    this.message.channel.send('At least one of the users does not exist');
+                  }
+                });
+              } else {
+                this.message.channel.send('You must enter a valid user tag');
+              }
+            } else {
+              this.message.channel.send('You are not allowed to make this transfer');
+            }
+          } else {
+            this.message.channel.send('You must enter a valid user tag');
+          }
+        } else {
+          this.message.channel.send('You must enter two user tags and a positive amount');
+        }
+      } else {
+        //TODO : display help
+      }
     }
   }
 }
